5d1eaf90fcaaaac446d9f4f1b1c68e0660aa5af2
naver0504 <qortndwls1234@gmail.com>
2024-07-08 09:11:55 +0900

    feat: PageSize 조정

--- src/main/java/com/example/comprehensivedegisn/api/OpenApiClient.java
+++ src/main/java/com/example/comprehensivedegisn/api/OpenApiClient.java
@@ -1,31 +1,35 @@
 package com.example.comprehensivedegisn.api;
 
 
-import com.example.comprehensivedegisn.api.dto.ApartmentDetailRootElement;
+import com.example.comprehensivedegisn.api.dto.ApartmentDetailResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDate;
+import java.util.Date;
 
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class OpenApiClient {
 
     private final OpenApiUtils openApiUtils;
+    private final int NUM_OF_ROWS = 1000;
 
-    public ApartmentDetailRootElement request(int pageNo, LocalDate contractDate) throws Exception {
-        RestClient restClient = RestClient.create();
+    public ApartmentDetailResponse request(int pageNo, LocalDate contractDate, String regionalCode) {
 
-        OpenApiRequest openApiRequest = OpenApiRequest.builder()
+        log.info("Reading page: {}", pageNo);
+        log.info("Reading contractDate: {}", contractDate);
+
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForObject(openApiUtils.createURI(OpenApiRequest.builder()
+                .LAWD_CD(regionalCode)
+                .numOfRows(NUM_OF_ROWS)
                 .pageNo(pageNo)
                 .contractDate(contractDate)
-                .build();
-
-        return openApiUtils.convertXmlToApartmentDetail(restClient.get()
-                .uri(openApiUtils.createURI(openApiRequest))
-                .retrieve()
-                .body(String.class));
+                .build()), ApartmentDetailResponse.class);
     }
 }
