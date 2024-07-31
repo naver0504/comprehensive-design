d8e259a003f408e125d4e7ac950dbaebc78edad3
naver0504 <qortndwls1234@gmail.com>
2024-08-01 08:01:35 +0900

    refactor: OpenApiUtils.java 와 OpenApiRequest.java 코드 OpenApiClient 로 이동

--- src/main/java/com/example/comprehensivedegisn/api/OpenApiClient.java
+++ src/main/java/com/example/comprehensivedegisn/api/OpenApiClient.java
@@ -2,34 +2,73 @@ package com.example.comprehensivedegisn.api;
 
 
 import com.example.comprehensivedegisn.api.dto.ApartmentDetailResponse;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-
+import java.net.URI;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
-import java.util.Date;
+import java.time.format.DateTimeFormatter;
 
 
 @Component
 @RequiredArgsConstructor
+@EnableConfigurationProperties(OpenAPiProperties.class)
 @Slf4j
 public class OpenApiClient {
 
-    private final OpenApiUtils openApiUtils;
     private final int NUM_OF_ROWS = 1000;
+    private final OpenAPiProperties openAPiProperties;
 
     public ApartmentDetailResponse request(int pageNo, LocalDate contractDate, String regionalCode) {
 
-        log.info("Reading page: {}", pageNo);
-        log.info("Reading contractDate: {}", contractDate);
-
         RestTemplate restTemplate = new RestTemplate();
-        return restTemplate.getForObject(openApiUtils.createURI(OpenApiRequest.builder()
-                .LAWD_CD(regionalCode)
+
+        OpenApiRequest openApiRequest = OpenApiRequest.builder()
                 .numOfRows(NUM_OF_ROWS)
+                .LAWD_CD(regionalCode)
                 .pageNo(pageNo)
                 .contractDate(contractDate)
-                .build()), ApartmentDetailResponse.class);
+                .build();
+
+        return restTemplate.getForObject(createOpenApiRequestUri(openApiRequest), ApartmentDetailResponse.class);
+    }
+
+    private URI createOpenApiRequestUri(OpenApiRequest openApiRequest) {
+        StringBuilder urlBuilder = new StringBuilder(openAPiProperties.endPoint());
+        urlBuilder.append("?").append("serviceKey=").append(openAPiProperties.serviceKey());
+        urlBuilder.append("&").append("numOfRows=").append(URLEncoder.encode(String.valueOf(openApiRequest.getNumOfRows()), StandardCharsets.UTF_8));
+        urlBuilder.append("&").append("LAWD_CD=").append(URLEncoder.encode(openApiRequest.getLAWD_CD(), StandardCharsets.UTF_8));
+        urlBuilder.append("&").append("pageNo=").append(URLEncoder.encode(String.valueOf(openApiRequest.getPageNo()), StandardCharsets.UTF_8));
+        urlBuilder.append("&").append("DEAL_YMD=").append(URLEncoder.encode(openApiRequest.getContractDate(), StandardCharsets.UTF_8));
+        return URI.create(urlBuilder.toString());
+    }
+
+    @Getter
+    private static class OpenApiRequest {
+
+        private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMM");
+
+        private final int numOfRows;
+        private final String LAWD_CD;
+        private final int pageNo;
+        private final String contractDate;
+
+        @Builder
+        public OpenApiRequest(int numOfRows, String LAWD_CD, int pageNo, LocalDate contractDate) {
+            this.numOfRows = numOfRows;
+            this.LAWD_CD = LAWD_CD;
+            this.pageNo = pageNo;
+            this.contractDate = convertLocalDateToString(contractDate);
+        }
+
+        public String convertLocalDateToString(LocalDate localDate) {
+            return formatter.format(localDate);
+        }
     }
 }
