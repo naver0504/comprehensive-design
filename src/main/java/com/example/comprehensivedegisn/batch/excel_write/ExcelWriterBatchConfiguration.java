f0954314d451b267b01c84c3257b7379cbc5a121
naver0504 <qortndwls1234@gmail.com>
2024-09-19 05:33:01 +0900

    feat: 파일생성 Batch 작성

---
+++ src/main/java/com/example/comprehensivedegisn/batch/excel_write/ExcelWriterBatchConfiguration.java
@@ -0,0 +1,88 @@
+package com.example.comprehensivedegisn.batch.excel_write;
+
+import com.example.comprehensivedegisn.batch.query_dsl.QuerydslNoOffsetIdPagingItemReader;
+import com.example.comprehensivedegisn.batch.query_dsl.expression.Expression;
+import com.example.comprehensivedegisn.batch.query_dsl.options.QuerydslNoOffsetNumberOptions;
+import com.example.comprehensivedegisn.domain.ApartmentTransaction;
+import com.example.comprehensivedegisn.domain.Gu;
+import jakarta.persistence.EntityManagerFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.example.comprehensivedegisn.domain.QApartmentTransaction.*;
+import static com.example.comprehensivedegisn.domain.QDongEntity.*;
+
+@Configuration
+@RequiredArgsConstructor
+public class ExcelWriterBatchConfiguration {
+
+
+    private final String JOB_NAME = "excelWriterJob";
+    private final String STEP_NAME = JOB_NAME + "_step";
+    private int chunkSize = 1000;
+    private final EntityManagerFactory emf;
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager platformTransactionManager;
+
+    @Bean
+    @StepScope
+    public QuerydslNoOffsetIdPagingItemReader<ApartmentTransaction, Long> apartmentTransactionReader(@Value("#{jobParameters[regionalCode]}") String regionalCode) {
+
+        QuerydslNoOffsetNumberOptions<ApartmentTransaction, Long> options =
+                new QuerydslNoOffsetNumberOptions<>(apartmentTransaction.id, Expression.ASC);
+        return new QuerydslNoOffsetIdPagingItemReader<>(emf, chunkSize, options, query -> query
+                .selectFrom(apartmentTransaction)
+                .innerJoin(dongEntity).on(apartmentTransaction.dongEntity.eq(dongEntity))
+                .where(dongEntity.guCode.eq(regionalCode)));
+    }
+
+    @Bean
+    @StepScope
+    public FlatFileItemWriter<ApartmentTransaction> apartmentTransactionExcelWriter(@Value("#{jobParameters[regionalCode]}") String regionalCode) {
+        Field[] fields = ApartmentTransaction.class.getDeclaredFields();
+        List<String> fieldList = Arrays.stream(fields).map(Field::getName).toList();
+        String[] fieldNames = fieldList.subList(1, fieldList.size() - 1).toArray(new String[0]);
+        return new FlatFileItemWriterBuilder<ApartmentTransaction>()
+                .name("apartmentTransactionWriter")
+                .resource(new FileSystemResource("C:\\Users\\qortm\\intelliJ\\comprehensive-degisn\\src\\main\\resources\\static" + "/apartment_transaction_" + Gu.getGu(regionalCode) + ".txt"))
+                .delimited()
+                .delimiter("|")
+                .names(fieldNames)
+                .shouldDeleteIfExists(true)
+                .build();
+    }
+
+    @Bean
+    public Job excelWriterJob() {
+        return new JobBuilder(JOB_NAME, jobRepository)
+                .incrementer(new RunIdIncrementer())
+                .start(excelWriterStep())
+                .build();
+    }
+
+    @Bean
+    public Step excelWriterStep() {
+        return new StepBuilder(STEP_NAME, jobRepository)
+                .<ApartmentTransaction, ApartmentTransaction>chunk(chunkSize, platformTransactionManager)
+                .reader(apartmentTransactionReader(null))
+                .writer(apartmentTransactionExcelWriter(null))
+                .build();
+    }
+}
---
+++ src/main/java/com/example/comprehensivedegisn/controller/ExcelWriteBatchController.java
@@ -0,0 +1,40 @@
+package com.example.comprehensivedegisn.controller;
+
+
+import com.example.comprehensivedegisn.domain.Gu;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.explore.JobExplorer;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/excel/write")
+public class ExcelWriteBatchController {
+
+    private final JobLauncher jobLauncher;
+    private final JobExplorer explorer;
+    private final BeanFactory beanFactory;
+
+
+    @GetMapping
+    public void writeExcel(@RequestParam("Gu") Gu gu) throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+        Job job = beanFactory.getBean("excelWriterJob", Job.class);
+        jobLauncher.run(job, new JobParametersBuilder(explorer)
+                .getNextJobParameters(job)
+                .addJobParameters(new JobParametersBuilder()
+                        .addString("regionalCode", gu.getRegionalCode())
+                        .toJobParameters()).toJobParameters());
+
+    }
+}
