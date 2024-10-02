2089e4a6fb8ca99e51e15c01497ca2bedd8a0de3
naver0504 <qortndwls1234@gmail.com>
2024-10-02 15:47:32 +0900

    refactor: package 위치 변경

--- src/test/java/com/example/comprehensivedegisn/api/BatchTest.java
+++ src/test/java/com/example/comprehensivedegisn/api/BatchTest.java
@@ -2,6 +2,7 @@ package com.example.comprehensivedegisn.api;
 
 import com.example.comprehensivedegisn.batch.open_api.JdbcTemplateConfig;
 import com.example.comprehensivedegisn.batch.open_api.OpenApiBatchConfiguration;
+import com.example.comprehensivedegisn.batch.open_api.OpenApiClient;
 import com.example.comprehensivedegisn.domain.Gu;
 import com.example.comprehensivedegisn.domain.repository.ApartmentTransactionRepository;
 import com.example.comprehensivedegisn.domain.repository.DongRepository;
@@ -9,35 +10,36 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.batch.core.*;
+import org.springframework.batch.core.explore.JobExplorer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.item.ExecutionContext;
 import org.springframework.batch.test.JobLauncherTestUtils;
-import org.springframework.batch.test.JobRepositoryTestUtils;
 import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 
 import static com.example.comprehensivedegisn.domain.Gu.노원구;
 
+@ActiveProfiles("prod")
 @SpringBootTest
 @ComponentScan(basePackageClasses = {OpenApiBatchConfiguration.class, DongRepository.class})
-@Import({OpenApiClient.class, OpenApiUtils.class, TestBatchConfig.class, JdbcTemplateConfig.class})
+@Import({OpenApiClient.class, TestBatchConfig.class, JdbcTemplateConfig.class})
 @SpringBatchTest
 public class BatchTest {
 
 
     @Autowired
     private ApartmentTransactionRepository apartmentTransactionRepository;
+    @Autowired
+    private JobExplorer jobExplorer;
 
     @Autowired
     private JobLauncherTestUtils jobLauncherTestUtils;
-
-    @Autowired
-    private JobRepositoryTestUtils jobRepositoryTestUtils;
 
     @Autowired
     private JobRepository jobRepository;
@@ -47,8 +49,8 @@ public class BatchTest {
 
     @AfterEach
     void tearDown() {
-        jobRepositoryTestUtils.removeJobExecutions();
-        apartmentTransactionRepository.deleteAll();
+//        jobRepositoryTestUtils.removeJobExecutions();
+//        apartmentTransactionRepository.deleteAll();
     }
 
     @Test
@@ -70,11 +72,10 @@ public class BatchTest {
     @Test
     void testJob() throws Exception {
 
-
         Job job = beanFactory.getBean("simpleOpenApiJob", Job.class);
 
         JobParametersBuilder jobParametersBuilder = new JobParametersBuilder();
-        jobParametersBuilder.addString("regionalCode", Gu.중구.getRegionalCode());
+        jobParametersBuilder.addString("regionalCode", Gu.송파구.getRegionalCode());
         JobParameters jobParameters = jobParametersBuilder.toJobParameters();
 
         jobLauncherTestUtils.setJob(job);
