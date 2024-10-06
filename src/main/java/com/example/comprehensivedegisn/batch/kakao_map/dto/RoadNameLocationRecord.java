0049e9fda289feaab6df932e8dbd61e326f6fd4c
naver0504 <qortndwls1234@gmail.com>
2024-10-07 04:05:56 +0900

    feat: toApartmentGeoRecord 함수 및 EMPTY RoadNameLocationRecord 추가(메모리 절약을 위해)

--- src/main/java/com/example/comprehensivedegisn/batch/kakao_map/dto/RoadNameLocationRecord.java
+++ src/main/java/com/example/comprehensivedegisn/batch/kakao_map/dto/RoadNameLocationRecord.java
@@ -1,4 +1,11 @@
 package com.example.comprehensivedegisn.batch.kakao_map.dto;
 
 
-public record RoadNameLocationRecord(String x, String y) { }
+public record RoadNameLocationRecord(String x, String y) {
+
+    public static final RoadNameLocationRecord EMPTY = new RoadNameLocationRecord(null, null);
+
+    public ApartmentGeoRecord toApartmentGeoRecord(long id) {
+        return new ApartmentGeoRecord(id, x, y);
+    }
+}
