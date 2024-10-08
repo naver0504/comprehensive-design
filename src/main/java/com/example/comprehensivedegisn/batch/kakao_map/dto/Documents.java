aacc697244587669ee3bc68a6082d4591657b872
naver0504 <qortndwls1234@gmail.com>
2024-10-09 07:13:12 +0900

    refactor: RoadNameLocationRecord -> LocationRecord 클래스 명 변경

--- src/main/java/com/example/comprehensivedegisn/batch/kakao_map/dto/Documents.java
+++ src/main/java/com/example/comprehensivedegisn/batch/kakao_map/dto/Documents.java
@@ -1,17 +1,16 @@
 package com.example.comprehensivedegisn.batch.kakao_map.dto;
 
-import com.example.comprehensivedegisn.domain.ApartmentTransaction;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
 
 public record Documents(List<Document> documents, Meta meta) {
 
-    public RoadNameLocationRecord toRoadNameLocationRecord() {
+    public LocationRecord toLocationRecord() {
         if(documents.isEmpty()) {
-            return RoadNameLocationRecord.EMPTY;
+            return LocationRecord.EMPTY;
         }
-        return new RoadNameLocationRecord(documents.get(0).x(), documents.get(0).y());
+        return new LocationRecord(documents.get(0).x(), documents.get(0).y());
     }
 
     public record Meta(@JsonProperty("is_end") boolean isEnd,
--- src/main/java/com/example/comprehensivedegisn/batch/kakao_map/dto/LocationRecord.java
+++ src/main/java/com/example/comprehensivedegisn/batch/kakao_map/dto/LocationRecord.java
@@ -1,9 +1,9 @@
 package com.example.comprehensivedegisn.batch.kakao_map.dto;
 
 
-public record RoadNameLocationRecord(String x, String y) {
+public record LocationRecord(String x, String y) {
 
-    public static final RoadNameLocationRecord EMPTY = new RoadNameLocationRecord(null, null);
+    public static final LocationRecord EMPTY = new LocationRecord(null, null);
 
     public ApartmentGeoRecord toApartmentGeoRecord(long id) {
         return new ApartmentGeoRecord(id, x, y);
