0803b8253070268cd7e91162ebc3e50dbabfeb1f
naver0504 <qortndwls1234@gmail.com>
2024-10-16 06:53:13 +0900

    feat: EnableBatchProcessing 어노테이션 추가

--- src/main/java/com/example/comprehensivedegisn/ComprehensiveDegisnApplication.java
+++ src/main/java/com/example/comprehensivedegisn/ComprehensiveDegisnApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class ComprehensiveDegisnApplication {
 
     public static void main(String[] args) {
