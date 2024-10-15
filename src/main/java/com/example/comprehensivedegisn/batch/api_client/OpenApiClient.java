39b5784ad744022eba52d2c672740cda54177ec1
naver0504 <qortndwls1234@gmail.com>
2024-10-16 06:48:11 +0900

    refactor: OpenApiClient ApiClient 인터페이스 상속으로 추상화

---
+++ src/main/java/com/example/comprehensivedegisn/batch/api_client/OpenApiClient.java
@@ -0,0 +1,34 @@
+package com.example.comprehensivedegisn.batch.api_client;
+
+
+import com.example.comprehensivedegisn.batch.open_api.OpenAPiProperties;
+import com.example.comprehensivedegisn.batch.open_api.dto.ApartmentDetailResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+
+
+@RequiredArgsConstructor
+@Slf4j
+public class OpenApiClient implements ApiClient<OpenApiRequest, ApartmentDetailResponse> {
+
+    private static final int NUM_OF_ROWS = 5;
+    private final OpenAPiProperties openAPiProperties;
+    private final RestTemplate restTemplate;
+
+
+    @Override
+    public String createUrl(OpenApiRequest openApiRequest) {
+        return openAPiProperties.endPoint() + "?" + "serviceKey=" + openAPiProperties.serviceKey() +
+                "&" + "numOfRows=" + NUM_OF_ROWS +
+                "&" + "LAWD_CD=" + openApiRequest.regionalCode() +
+                "&" + "pageNo=" + openApiRequest.pageNo() +
+                "&" + "DEAL_YMD=" + openApiRequest.contractDate();    }
+
+    @Override
+    public ApartmentDetailResponse callApi(OpenApiRequest openApiRequest) {
+        return restTemplate.getForObject(URI.create(createUrl(openApiRequest)), ApartmentDetailResponse.class);
+    }
+}
