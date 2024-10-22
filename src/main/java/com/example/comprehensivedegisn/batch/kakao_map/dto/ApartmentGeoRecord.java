c27a09bfb05eb26482eb215c258dc275401fec2e
naver0504 <qortndwls1234@gmail.com>
2024-10-23 02:39:18 +0900

    feat: mysql Point 형식으로 변환하는 Method 추가

--- src/main/java/com/example/comprehensivedegisn/batch/kakao_map/dto/ApartmentGeoRecord.java
+++ src/main/java/com/example/comprehensivedegisn/batch/kakao_map/dto/ApartmentGeoRecord.java
@@ -1,5 +1,17 @@
 package com.example.comprehensivedegisn.batch.kakao_map.dto;
 
+
 public record ApartmentGeoRecord (Long id, String x, String y) {
 
+    public boolean isNotEmpty() {
+        return x != null && y != null;
+    }
+
+    public String toPoint() {
+        if(x == null || y == null) {
+            return String.format("POINT(%s %s)", "0", "0");
+        } else {
+            return String.format("POINT(%s %s)", x, y);
+        }
+    }
 }
