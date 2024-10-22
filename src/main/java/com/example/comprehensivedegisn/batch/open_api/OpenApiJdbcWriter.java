eabba432abab2db8fe59336e0548b5eb7329dd13
naver0504 <qortndwls1234@gmail.com>
2024-10-23 02:46:00 +0900

    refactor: ApartmentTransaction Entity 수정에 따른 쿼리 변경

--- src/main/java/com/example/comprehensivedegisn/batch/open_api/OpenApiJdbcWriter.java
+++ src/main/java/com/example/comprehensivedegisn/batch/open_api/OpenApiJdbcWriter.java
@@ -3,10 +3,8 @@ package com.example.comprehensivedegisn.batch.open_api;
 
 import com.example.comprehensivedegisn.batch.open_api.dto.ApartmentDetail;
 import com.example.comprehensivedegisn.batch.open_api.dto.ApartmentDetailResponse;
-import com.example.comprehensivedegisn.domain.DongEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.item.*;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
 
@@ -20,9 +18,6 @@ public class OpenApiJdbcWriter implements ItemWriter<ApartmentDetailResponse> {
     private final OpenApiDongDataHolder openApiDongDataHolder;
     private final JdbcTemplate jdbcTemplate;
 
-    @Value("#{jobParameters[regionalCode]}")
-    private String regionalCode;
-
     private final String INSERT_SQL =
             "INSERT INTO apartment_transaction (" +
                     "deal_amount, " +
@@ -32,18 +27,14 @@ public class OpenApiJdbcWriter implements ItemWriter<ApartmentDetailResponse> {
                     "road_name_bonbun, " +
                     "road_name_bubun, " +
                     "road_name_code, " +
-                    "dong, " +
                     "bonbun, " +
                     "bubun, " +
-                    "land_code, " +
                     "apartment_name, " +
                     "deal_month, " +
                     "deal_day, " +
                     "area_for_exclusive_use, " +
                     "jibun, " +
                     "floor, " +
-                    "registration_date, " +
-                    "rdealer_law_dnm, " +
                     "dong_entity_id" +
                     ") " +
                     "VALUES " +
@@ -67,24 +58,19 @@ public class OpenApiJdbcWriter implements ItemWriter<ApartmentDetailResponse> {
                 ps.setString(1, apartmentDetail.dealAmount().trim());
                 ps.setInt(2, apartmentDetail.buildYear());
                 ps.setInt(3, apartmentDetail.dealYear());
-                ps.setString(4, apartmentDetail.roadName().trim());
+                ps.setString(4, apartmentDetail.roadName() != null ? apartmentDetail.roadName().trim() : null);
                 ps.setInt(5, apartmentDetail.roadNameBonbun());
                 ps.setInt(6, apartmentDetail.roadNameBubun());
                 ps.setInt(7, apartmentDetail.roadNameCode());
-                ps.setString(8, apartmentDetail.dong().trim());
-                ps.setInt(9, apartmentDetail.bonbun());
-                ps.setInt(10, apartmentDetail.bubun());
-                ps.setInt(11, apartmentDetail.landCode());
-                ps.setString(12, apartmentDetail.apartmentName().trim());
-                ps.setInt(13, apartmentDetail.dealMonth());
-                ps.setInt(14, apartmentDetail.dealDay());
-                ps.setDouble(15, apartmentDetail.areaForExclusiveUse());
-                ps.setString(16, apartmentDetail.jibun().trim());
-                ps.setInt(17, apartmentDetail.floor());
-                ps.setString(18, apartmentDetail.registrationDate());
-                ps.setString(19, apartmentDetail.rdealerLawDnm());
-                DongEntity dongEntity = openApiDongDataHolder.getDongEntity(regionalCode, apartmentDetail.eupmyeondongCode().trim());
-                ps.setLong(20, dongEntity.getId());
+                ps.setInt(8, apartmentDetail.bonbun());
+                ps.setInt(9, apartmentDetail.bubun());
+                ps.setString(10, apartmentDetail.apartmentName().trim());
+                ps.setInt(11, apartmentDetail.dealMonth());
+                ps.setInt(12, apartmentDetail.dealDay());
+                ps.setDouble(13, apartmentDetail.areaForExclusiveUse());
+                ps.setString(14, apartmentDetail.jibun().trim());
+                ps.setInt(15, apartmentDetail.floor());
+                ps.setLong(16, openApiDongDataHolder.getDongEntityId(apartmentDetail.eupmyeondongCode().trim()));
             }
 
             @Override
