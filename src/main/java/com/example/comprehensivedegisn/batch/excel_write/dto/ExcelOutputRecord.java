0d82e277a35144061b05901a1f3d84659262a0c5
naver0504 <qortndwls1234@gmail.com>
2024-10-28 05:27:50 +0900

    feat: ExcelOutput 데이터 집합 추가

---
+++ src/main/java/com/example/comprehensivedegisn/batch/excel_write/dto/ExcelOutputRecord.java
@@ -0,0 +1,21 @@
+package com.example.comprehensivedegisn.batch.excel_write.dto;
+
+import com.example.comprehensivedegisn.domain.Gu;
+
+import java.time.LocalDate;
+
+public record ExcelOutputRecord(
+        LocalDate dealDate,
+        double interestRate,
+        Gu gu,
+        String dong,
+        double exclusiveArea,
+        int floor,
+        int buildYear,
+        Double x,
+        Double y,
+        String dealAmount,
+        String roadNameAddress
+) {
+
+}
