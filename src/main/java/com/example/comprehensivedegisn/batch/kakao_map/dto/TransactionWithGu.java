f4596f8eb103ec8a39b78296a82f879f65783942
naver0504 <qortndwls1234@gmail.com>
2024-10-28 05:31:07 +0900

    refactor: AddressUtils 사용으로 변경

--- src/main/java/com/example/comprehensivedegisn/batch/kakao_map/dto/TransactionWithGu.java
+++ src/main/java/com/example/comprehensivedegisn/batch/kakao_map/dto/TransactionWithGu.java
@@ -1,10 +1,11 @@
 package com.example.comprehensivedegisn.batch.kakao_map.dto;
 
+import com.example.comprehensivedegisn.domain.AddressUtils;
 import com.example.comprehensivedegisn.domain.Gu;
 
 public record TransactionWithGu(long id, Gu gu, String dong, String jibun) {
 
     public String getJibunAddress() {
-        return gu + " " + dong + " " + jibun;
+        return AddressUtils.getJibunAddress(gu, dong, jibun);
     }
 }
