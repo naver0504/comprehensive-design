3efb5d27375b812733f6f4edf55f2fe6a1c6c0c9
naver0504 <qortndwls1234@gmail.com>
2024-10-28 06:10:28 +0900

    refactor: Optional 반환으로 메소드 수정

--- src/main/java/com/example/comprehensivedegisn/batch/kakao_map/dto/TransactionWithGu.java
+++ src/main/java/com/example/comprehensivedegisn/batch/kakao_map/dto/TransactionWithGu.java
@@ -3,9 +3,15 @@ package com.example.comprehensivedegisn.batch.kakao_map.dto;
 import com.example.comprehensivedegisn.domain.AddressUtils;
 import com.example.comprehensivedegisn.domain.Gu;
 
+import java.util.Optional;
+
 public record TransactionWithGu(long id, Gu gu, String dong, String jibun) {
 
-    public String getJibunAddress() {
+    public Optional<String> getJibunAddress() {
         return AddressUtils.getJibunAddress(gu, dong, jibun);
+    }
+
+    public String getResolvedJibunAddress() {
+        return getJibunAddress().orElseThrow();
     }
 }
