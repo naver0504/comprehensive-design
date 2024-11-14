ce6124c9f27810d69a88b04dd621c8325dc57203
naver0504 <qortndwls1234@gmail.com>
2024-11-15 05:55:17 +0900

    fix: 바뀐 api에 따라 RoadName과 관련된 메소드 Deprecate 처리

--- src/main/java/com/example/comprehensivedegisn/adapter/domain/AddressUtils.java
+++ src/main/java/com/example/comprehensivedegisn/adapter/domain/AddressUtils.java
@@ -14,27 +14,32 @@ public final class AddressUtils {
         return gu + " " + dong + " " + jibun;
     }
 
+    @Deprecated
     public static String getRoadNameWithSeoulPrefixAndGu(Gu gu, String roadName, int roadNameBonbun, int roadNameBubun) {
         if(roadName == null) return null;
         return SEOUL + " " + getRoadNameWithGu(gu, roadName, roadNameBonbun, roadNameBubun);
     }
 
+    @Deprecated
     public static String getRoadNameWithGu(Gu gu, String roadName, int roadNameBonbun, int roadNameBubun) {
         if(roadName == null) return null;
 
         return  gu + " " + concatRoadName(roadName, roadNameBonbun, roadNameBubun);
     }
 
+    @Deprecated
     public static Optional<String> getRoadName(String roadName, int roadNameBonbun, int roadNameBubun) {
         if(roadName == null || roadNameBonbun == 0) return Optional.empty();
         return Optional.of(concatRoadName(roadName, roadNameBonbun, roadNameBubun));
     }
 
+    @Deprecated
     private static String concatRoadName(String roadName, int roadNameBonbun, int roadNameBubun) {
         if(roadNameBonbun == 0) return roadName;
         return roadName + " " + roadNameBonbun  + getRoadNameBubun(roadNameBubun);
     }
 
+    @Deprecated
     private static String getRoadNameBubun(int roadNameBubun) {
         return roadNameBubun == 0 ? "" : "-" + roadNameBubun;
     }
