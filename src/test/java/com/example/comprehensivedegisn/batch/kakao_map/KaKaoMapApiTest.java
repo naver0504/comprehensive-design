0557d77e92818d4f328deb29cab50e56d28267de
naver0504 <qortndwls1234@gmail.com>
2024-11-15 03:22:53 +0900

    fix: 오타 수정

--- src/test/java/com/example/comprehensivedegisn/batch/kakao_map/KaKaoMapApiTest.java
+++ src/test/java/com/example/comprehensivedegisn/batch/kakao_map/KaKaoMapApiTest.java
@@ -10,7 +10,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
--import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
