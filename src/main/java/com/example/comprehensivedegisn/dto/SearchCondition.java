42c420698b5fe91d82efc7528678c58273e4aef4
naver0504 <qortndwls1234@gmail.com>
2024-11-18 22:50:04 +0900

    feat: PredictCost 관련 필드 및 validate 메소드 추가

--- src/main/java/com/example/comprehensivedegisn/dto/SearchCondition.java
+++ src/main/java/com/example/comprehensivedegisn/dto/SearchCondition.java
@@ -1,24 +1,60 @@
 package com.example.comprehensivedegisn.dto;
 
 import com.example.comprehensivedegisn.adapter.domain.Gu;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import lombok.*;
 
 import java.time.LocalDate;
 
+import static com.example.comprehensivedegisn.adapter.domain.QPredictCost.predictCost;
+
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @AllArgsConstructor
-@ToString
 public class SearchCondition {
 
     private Gu gu = Gu.NONE;
     private String dong;
     private String apartmentName;
     private Double areaForExclusiveUse;
+
     private LocalDate startDealDate;
     private LocalDate endDealDate;
 
-    public boolean validate() {
-        return gu != Gu.NONE || dong == null;
+    private Reliability reliability;
+
+    public BooleanExpression toReliabilityEq() {
+        return reliability.reliabilityExpression;
+    }
+
+    public boolean isNotValid() {
+        return isDongNotValidate() || isApartmentNameNotValid() || isAreaNotValid();
+    }
+
+    private boolean isAreaNotValid() {
+        return apartmentName == null && areaForExclusiveUse != null;
+    }
+
+    private boolean isApartmentNameNotValid() {
+        return dong == null && apartmentName != null;
+    }
+
+    private boolean isDongNotValidate() {
+        return gu == Gu.NONE && dong != null;
+    }
+
+
+    @Getter
+    public enum Reliability {
+        ALL(predictCost.isReliable.in(true, false)),
+        RELIABLE(predictCost.isReliable.isTrue()),
+        UNRELIABLE(predictCost.isReliable.isFalse());
+
+        private final BooleanExpression reliabilityExpression;
+
+        Reliability(BooleanExpression reliabilityExpression) {
+            this.reliabilityExpression = reliabilityExpression;
+        }
+
     }
 }
