e91d92df919884e3a4d7d2c259b26f8284cb8ed5
naver0504 <qortndwls1234@gmail.com>
2024-11-18 22:50:32 +0900

    test: 필터링이 제대로 되었는 지에 대한 검증 로직 추가

--- src/test/java/com/example/comprehensivedegisn/adapter/repository/apart/QuerydslApartmentTransactionRepositoryTest.java
+++ src/test/java/com/example/comprehensivedegisn/adapter/repository/apart/QuerydslApartmentTransactionRepositoryTest.java
@@ -5,8 +5,7 @@ import com.example.comprehensivedegisn.adapter.order.CustomPageable;
 import com.example.comprehensivedegisn.adapter.order.OrderType;
 import com.example.comprehensivedegisn.adapter.repository.BaseRepositoryTest;
 import com.example.comprehensivedegisn.dto.SearchCondition;
-import com.example.comprehensivedegisn.dto.TransactionResponseRecord;
-import org.assertj.core.api.Assertions;
+import com.example.comprehensivedegisn.dto.SearchResponseRecord;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -14,9 +13,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 @BaseRepositoryTest
 class QuerydslApartmentTransactionRepositoryTest {
@@ -29,28 +29,67 @@ class QuerydslApartmentTransactionRepositoryTest {
     void searchTest(Long cachedCount, SearchCondition searchCondition, CustomPageable customPageable) {
 
         // when
-        Page<TransactionResponseRecord> result = querydslApartmentTransactionRepository.searchApartmentTransactions(cachedCount, searchCondition, customPageable);
+        Page<SearchResponseRecord> result = querydslApartmentTransactionRepository.searchApartmentTransactions(cachedCount, searchCondition, customPageable);
+        List<SearchResponseRecord> contents = result.getContent();
         // then
-        Assertions.assertThat(result.getSize()).isEqualTo(CustomPageable.DEFAULT_SIZE);
-        if(searchCondition.getStartDealDate() != null && searchCondition.getEndDealDate() != null) {
-            Assertions.assertThat(result.getContent())
-                    .extracting(TransactionResponseRecord::dealDate)
-                    .allMatch(dealDate -> dealDate.isAfter(searchCondition.getStartDealDate()) || dealDate.isEqual(searchCondition.getStartDealDate()))
-                    .allMatch(dealDate -> dealDate.isBefore(searchCondition.getEndDealDate()) || dealDate.isEqual(searchCondition.getEndDealDate()));;
-        }
-        if(cachedCount != null) {
-            Assertions.assertThat(result.getTotalElements()).isEqualTo(cachedCount);
-        }
+        
+        assertThat(result.getSize()).isEqualTo(CustomPageable.DEFAULT_SIZE);
+        assertThatGuEq(searchCondition, contents);
+        assertThatDongEq(searchCondition, contents);
+        asserThatAptNameEq(searchCondition, contents);
+        assertThatLocalDateBetween(searchCondition, contents);
+        assertThatCacheEq(cachedCount, result);
     }
 
     private static Stream<Arguments> searchStream() {
         return Stream.of(
-                Arguments.of(100L , new SearchCondition(Gu.마포구, null, null, null, LocalDate.of(2021, 1, 1), LocalDate.of(2021, 12, 31)), new CustomPageable(OrderType.DEAL_DATE, 3)),
-                Arguments.of(null , new SearchCondition(Gu.마포구, "아현동", "마포센트럴 아이파크", 111.1083, LocalDate.of(2021, 1, 1), LocalDate.of(2021, 12, 31)), new CustomPageable(OrderType.DEAL_AMOUNT, 0)),
-                Arguments.of(100L , new SearchCondition(Gu.마포구, null, "마포센트럴 아이파크", null, LocalDate.of(2021, 1, 1), null), new CustomPageable(OrderType.DEAL_AMOUNT, 3)),
-                Arguments.of(null , new SearchCondition(Gu.마포구, "아현동", null, 111.1083, null, null), new CustomPageable(OrderType.DEAL_AMOUNT, 0)),
-                Arguments.of(100L , new SearchCondition(Gu.NONE, null, "마포센트럴 아이파크", null, null, null), new CustomPageable(OrderType.DEAL_AMOUNT, 3))
+                Arguments.of(100L , new SearchCondition(Gu.마포구, null, null, null, LocalDate.of(2021, 1, 1), LocalDate.of(2021, 12, 31), SearchCondition.Reliability.ALL), new CustomPageable(OrderType.DEAL_DATE, 3)),
+                Arguments.of(null , new SearchCondition(Gu.마포구, "아현동", "마포센트럴 아이파크", 111.1083, LocalDate.of(2021, 1, 1), LocalDate.of(2021, 12, 31), SearchCondition.Reliability.UNRELIABLE), new CustomPageable(OrderType.DEAL_AMOUNT, 0)),
+                Arguments.of(100L , new SearchCondition(Gu.마포구, null, "마포센트럴 아이파크", null, LocalDate.of(2021, 1, 1), null, SearchCondition.Reliability.RELIABLE), new CustomPageable(OrderType.DEAL_AMOUNT, 3)),
+                Arguments.of(null , new SearchCondition(Gu.마포구, "아현동", null, 111.1083, null, null, SearchCondition.Reliability.ALL), new CustomPageable(OrderType.DEAL_AMOUNT, 0)),
+                Arguments.of(100L , new SearchCondition(Gu.NONE, null, "마포센트럴 아이파크", null, null, null, SearchCondition.Reliability.UNRELIABLE), new CustomPageable(OrderType.DEAL_AMOUNT, 3))
 
         );
     }
+
+    private static void assertThatCacheEq(Long cachedCount, Page<SearchResponseRecord> result) {
+        if(cachedCount != null) {
+            assertThat(result.getTotalElements()).isEqualTo(cachedCount);
+        }
+    }
+
+    private void assertThatLocalDateBetween(SearchCondition searchCondition, List<SearchResponseRecord> contents) {
+        if(searchCondition.getStartDealDate() != null && searchCondition.getEndDealDate() != null) {
+            assertThat(contents)
+                    .extracting(SearchResponseRecord::dealDate)
+                    .allMatch(dealDate -> dealDate.isAfter(searchCondition.getStartDealDate()) || dealDate.isEqual(searchCondition.getStartDealDate()))
+                    .allMatch(dealDate -> dealDate.isBefore(searchCondition.getEndDealDate()) || dealDate.isEqual(searchCondition.getEndDealDate()));;
+        }
+    }
+
+    private void asserThatAptNameEq(SearchCondition searchCondition, List<SearchResponseRecord> contents) {
+        if(searchCondition.getApartmentName() != null) {
+            assertThat(contents)
+                    .extracting(SearchResponseRecord::apartmentName)
+                    .allMatch(apartmentName -> apartmentName.contains(searchCondition.getApartmentName()));
+        }
+    }
+
+    private void assertThatDongEq(SearchCondition searchCondition, List<SearchResponseRecord> contents) {
+        if(searchCondition.getDong() != null) {
+            assertThat(contents)
+                    .extracting(SearchResponseRecord::region)
+                    .allMatch(region -> region.contains(searchCondition.getDong()));
+        }
+    }
+
+    private void assertThatGuEq(SearchCondition searchCondition, List<SearchResponseRecord> contents) {
+        if(searchCondition.getGu() != Gu.NONE) {
+            assertThat(contents)
+                    .extracting(SearchResponseRecord::region)
+                    .allMatch(region -> region.contains(searchCondition.getGu().toString()));
+        }
+    }
+
+
 }
