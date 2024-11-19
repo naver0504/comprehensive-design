9e9ef112acd037590f287e808e39117e1bca9cea
naver0504 <qortndwls1234@gmail.com>
2024-11-20 07:09:26 +0900

    feat: Parsing을 위한 InitBinder

--- src/main/java/com/example/comprehensivedegisn/controller/ApartmentTransactionController.java
+++ src/main/java/com/example/comprehensivedegisn/controller/ApartmentTransactionController.java
@@ -7,14 +7,23 @@ import com.example.comprehensivedegisn.service.ApartmentTransactionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class ApartmentTransactionController {
+
+
+    /**
+     * WebDataBinder 통해 직접 필드에 접근할 수 있도록 설정
+     * Setter 통해 바인딩하지 않고 필드에 직접 접근하여 바인딩
+     *
+     */
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        dataBinder.initDirectFieldAccess();
+    }
 
     private final ApartmentTransactionService apartmentTransactionService;
 
@@ -22,6 +31,7 @@ public class ApartmentTransactionController {
     public ResponseEntity<Page<SearchResponseRecord>> searchApartmentTransactions(@RequestParam(required = false) Long cachedCount,
                                                                                   @ModelAttribute SearchCondition searchCondition,
                                                                                   @ModelAttribute CustomPageable customPageable) {
+
         return ResponseEntity.ok(apartmentTransactionService.searchApartmentTransactions(cachedCount, searchCondition, customPageable));
     }
 
