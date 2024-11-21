915f77ca183e57fe7adefb61d4bde63089135c77
naver0504 <qortndwls1234@gmail.com>
2024-11-22 03:32:09 +0900

    feat: ApartmentTransactionAdapter findApartmentNames, findAreaForExclusive 구현

--- src/main/java/com/example/comprehensivedegisn/adapter/impl/ApartmentTransactionAdapterImpl.java
+++ src/main/java/com/example/comprehensivedegisn/adapter/impl/ApartmentTransactionAdapterImpl.java
@@ -2,15 +2,19 @@ package com.example.comprehensivedegisn.adapter.impl;
 
 import com.example.comprehensivedegisn.adapter.ApartmentTransactionAdapter;
 import com.example.comprehensivedegisn.adapter.domain.ApartmentTransaction;
+import com.example.comprehensivedegisn.adapter.domain.Gu;
 import com.example.comprehensivedegisn.adapter.order.CustomPageable;
 import com.example.comprehensivedegisn.adapter.repository.apart.ApartmentTransactionRepository;
 import com.example.comprehensivedegisn.adapter.repository.apart.QuerydslApartmentTransactionRepository;
+import com.example.comprehensivedegisn.dto.SearchApartNameResponse;
+import com.example.comprehensivedegisn.dto.SearchAreaResponse;
 import com.example.comprehensivedegisn.dto.SearchCondition;
 import com.example.comprehensivedegisn.dto.SearchResponseRecord;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @RequiredArgsConstructor
@@ -28,6 +32,16 @@ public class ApartmentTransactionAdapterImpl implements ApartmentTransactionAdap
     @Override
     public Page<SearchResponseRecord> searchApartmentTransactions(Long cachedCount, SearchCondition searchCondition, CustomPageable customPageable) {
         return querydslApartmentTransactionRepository.searchApartmentTransactions(cachedCount, searchCondition, customPageable);
+    }
+
+    @Override
+    public List<SearchApartNameResponse> findApartmentNames(Gu gu, String dongName) {
+        return querydslApartmentTransactionRepository.findApartmentNames(gu, dongName);
+    }
+
+    @Override
+    public List<SearchAreaResponse> findAreaForExclusive(Gu gu, String dongName, String apartmentName) {
+        return querydslApartmentTransactionRepository.findAreaForExclusive(gu, dongName, apartmentName);
     }
 
 
