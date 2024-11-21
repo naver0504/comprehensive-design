ebbbc65b79115c9ab265edcf3f35d401d12ff24c
naver0504 <qortndwls1234@gmail.com>
2024-11-22 03:32:46 +0900

    test: ApartmentTransactionService findApartmentNames, findAreaForExclusive 테스트 작성

--- src/test/java/com/example/comprehensivedegisn/service/integration/ApartmentTransactionServiceIntegrationTest.java
+++ src/test/java/com/example/comprehensivedegisn/service/integration/ApartmentTransactionServiceIntegrationTest.java
@@ -6,17 +6,18 @@ import com.example.comprehensivedegisn.adapter.order.OrderType;
 import com.example.comprehensivedegisn.adapter.repository.apart.ApartmentTransactionRepository;
 import com.example.comprehensivedegisn.adapter.repository.dong.DongRepository;
 import com.example.comprehensivedegisn.adapter.repository.predict_cost.PredictCostRepository;
-import com.example.comprehensivedegisn.dto.Reliability;
-import com.example.comprehensivedegisn.dto.SearchCondition;
-import com.example.comprehensivedegisn.dto.SearchResponseRecord;
+import com.example.comprehensivedegisn.dto.*;
 import com.example.comprehensivedegisn.service.ApartmentTransactionService;
 import com.example.comprehensivedegisn.service.integration.config.IntegrationTestForService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.example.comprehensivedegisn.adapter.repository.apart.QuerydslApartmentTransactionRepositoryTest.*;
@@ -54,16 +55,96 @@ public class ApartmentTransactionServiceIntegrationTest {
         Assertions.assertThat(contents).isEqualTo(expectedContents);
     }
 
-    @Test
-    void searchApartmentTransactions_With_Not_Valid_Input() {
+    @ParameterizedTest
+    @MethodSource("com.example.comprehensivedegisn.service.unit.ApartmentTransactionServiceUnitTest#provideNotValidSearchCondition")
+    void searchApartmentTransactions_With_Not_Valid_Input(Long count, SearchCondition notValidSearchCondition) {
         // given
-        Long count = 100L;
-        SearchCondition notValidSearchCondition = new SearchCondition(Gu.마포구, "test", null, 111.1083, null, null, Reliability.ALL);
-        CustomPageable customPageable = new CustomPageable(OrderType.DEAL_DATE, 3);
+       CustomPageable customPageable = new CustomPageable(OrderType.DEAL_DATE, 3);
 
         // when
         Assertions.assertThatThrownBy(() -> target.searchApartmentTransactions(count, notValidSearchCondition, customPageable))
-                .isInstanceOf(IllegalStateException.class);
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void findApartmentNames_With_Valid_Input() {
+        // given
+        Gu gu = Gu.서초구;
+        String dong = TEST_DONG;
+        String aptName = TEST_APT_NAME;
+
+        int repeat = 3;
+        DongEntity dongEntity = dongRepository.save(DongEntity.builder()
+                .gu(gu)
+                .dongName(dong)
+                .build());
+        for (int i = 0; i < repeat; i++) {
+            apartmentTransactionRepository.save(ApartmentTransaction.builder()
+                    .apartmentName(aptName + i)
+                    .dongEntity(dongEntity)
+                    .build());
+        }
+
+        SearchApartNameRequest request = new SearchApartNameRequest(gu, dong);
+
+        // when
+        List<SearchApartNameResponse> result = target.findApartmentNames(request);
+        // then
+        Assertions.assertThat(result).hasSize(repeat);
+        Assertions.assertThat(result)
+                .extracting(SearchApartNameResponse::apartmentName)
+                .allMatch(name -> name.startsWith(aptName));
+    }
+
+    @ParameterizedTest
+    @MethodSource("com.example.comprehensivedegisn.service.unit.ApartmentTransactionServiceUnitTest#provideNotValidSearchApartNameRequest")
+    void findApartmentNames_With_Not_Valid_Input(SearchApartNameRequest request) {
+        Assertions.assertThatThrownBy(() -> target.findApartmentNames(request))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+    @Test
+    void findAreaForExclusive_With_Valid_Input() {
+        // given
+        Gu gu = Gu.서초구;
+        String dong = TEST_DONG;
+        String aptName = TEST_APT_NAME;
+        double area = TEST_AREA;
+
+        int repeat = 3;
+        List<Double> expected = new ArrayList<>(repeat);
+
+        DongEntity dongEntity = dongRepository.save(DongEntity.builder()
+                .gu(gu)
+                .dongName(dong)
+                .build());
+        for (int i = 0; i < repeat; i++) {
+            double areaForExclusiveUse = area * i;
+            expected.add(areaForExclusiveUse);
+            apartmentTransactionRepository.save(ApartmentTransaction.builder()
+                    .apartmentName(aptName)
+                    .areaForExclusiveUse(areaForExclusiveUse)
+                    .dongEntity(dongEntity)
+                    .build());
+        }
+
+        SearchAreaRequest request = new SearchAreaRequest(gu, dong, aptName);
+
+        // when
+        List<SearchAreaResponse> result = target.findAreaForExclusive(request);
+        // then
+        Assertions.assertThat(result).hasSize(repeat);
+        Assertions.assertThat(result)
+                .extracting(SearchAreaResponse::areaForExclusiveUse)
+                .containsExactlyElementsOf(expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("com.example.comprehensivedegisn.service.unit.ApartmentTransactionServiceUnitTest#provideNotValidSearchAreaRequest")
+    void findAreaForExclusive_With_Not_Valid_Input(SearchAreaRequest request) {
+        Assertions.assertThatThrownBy(() -> target.findAreaForExclusive(request))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
 
@@ -105,4 +186,5 @@ public class ApartmentTransactionServiceIntegrationTest {
                 new SearchResponseRecord(firstAT.getId(), aptName, gu, dong, firstAT.getAreaForExclusiveUse(), firstAT.getDealDate(), firstAT.getDealAmount(), firstPC.getPredictedCost(), firstPC.isReliable()),
                 new SearchResponseRecord(secondAT.getId(), aptName, gu, dong, secondAT.getAreaForExclusiveUse(), secondAT.getDealDate(), secondAT.getDealAmount(), secondPC.getPredictedCost(), secondPC.isReliable()));
     }
+
 }
--- src/test/java/com/example/comprehensivedegisn/service/unit/ApartmentTransactionServiceUnitTest.java
+++ src/test/java/com/example/comprehensivedegisn/service/unit/ApartmentTransactionServiceUnitTest.java
@@ -4,13 +4,14 @@ import com.example.comprehensivedegisn.adapter.ApartmentTransactionAdapter;
 import com.example.comprehensivedegisn.adapter.domain.Gu;
 import com.example.comprehensivedegisn.adapter.order.CustomPageable;
 import com.example.comprehensivedegisn.adapter.order.OrderType;
-import com.example.comprehensivedegisn.dto.Reliability;
-import com.example.comprehensivedegisn.dto.SearchCondition;
-import com.example.comprehensivedegisn.dto.SearchResponseRecord;
+import com.example.comprehensivedegisn.dto.*;
 import com.example.comprehensivedegisn.service.ApartmentTransactionService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -19,6 +20,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 @ExtendWith(MockitoExtension.class)
 public class ApartmentTransactionServiceUnitTest {
@@ -28,6 +30,7 @@ public class ApartmentTransactionServiceUnitTest {
 
     @Mock
     private ApartmentTransactionAdapter apartmentTransactionAdapter;
+
 
     @Test
     void searchApartmentTransactions_With_Valid_Input() {
@@ -51,18 +54,97 @@ public class ApartmentTransactionServiceUnitTest {
         BDDMockito.verify(apartmentTransactionAdapter, BDDMockito.times(1)).searchApartmentTransactions(count, searchCondition, customPageable);
     }
 
-    @Test
-    void searchApartmentTransactions_With_Not_Valid_Input() {
+    @ParameterizedTest
+    @MethodSource("provideNotValidSearchCondition")
+    void searchApartmentTransactions_With_Not_Valid_Input(Long count, SearchCondition notValidSearchCondition) {
         // given
-        Long count = 100L;
-        SearchCondition notValidSearchCondition = new SearchCondition(Gu.마포구, null, "마포센트럴 아이파크", 111.1083, null, null, Reliability.ALL);
         CustomPageable customPageable = new CustomPageable(OrderType.DEAL_DATE, 3);
-
         // when & then
         Assertions.assertThatThrownBy(() -> apartmentTransactionService.searchApartmentTransactions(count, notValidSearchCondition, customPageable))
-                .isInstanceOf(IllegalStateException.class);
+                .isInstanceOf(IllegalArgumentException.class);
         BDDMockito.verify(apartmentTransactionAdapter, BDDMockito.times(0)).searchApartmentTransactions(count, notValidSearchCondition, customPageable);
     }
 
+    @Test
+    void findApartmentNames_With_Valid_Input() {
+        // given
+        String dongName = "아현동";
+        SearchApartNameRequest request = new SearchApartNameRequest(Gu.마포구, dongName);
+        List<SearchApartNameResponse> expected = List.of(
+                new SearchApartNameResponse("마포센트럴 아이파크"),
+                new SearchApartNameResponse("마포센트럴 아이파크2")
+        );
+        BDDMockito.given(apartmentTransactionAdapter.findApartmentNames(request.getGu(), request.getDong())).willReturn(expected);
 
+        // when
+        List<SearchApartNameResponse> result = apartmentTransactionService.findApartmentNames(request);
+
+        // then
+        Assertions.assertThat(result).isEqualTo(expected);
+        BDDMockito.verify(apartmentTransactionAdapter, BDDMockito.times(1)).findApartmentNames(request.getGu(), request.getDong());
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideNotValidSearchApartNameRequest")
+    void findApartmentNames_With_Not_Valid_Input(SearchApartNameRequest request) {
+        // when & then
+        Assertions.assertThatThrownBy(() -> apartmentTransactionService.findApartmentNames(request))
+                .isInstanceOf(IllegalArgumentException.class);
+        BDDMockito.verify(apartmentTransactionAdapter, BDDMockito.times(0)).findApartmentNames(request.getGu(), request.getDong());
+    }
+
+    @Test
+    void findAreaForExclusive_With_Valid_Input() {
+        // given
+        SearchAreaRequest request = new SearchAreaRequest(Gu.마포구, "아현동", "마포센트럴 아이파크");
+        List<SearchAreaResponse> expected = List.of(
+                new SearchAreaResponse(100.1),
+                new SearchAreaResponse(100.2)
+        );
+        BDDMockito.given(apartmentTransactionAdapter.findAreaForExclusive(request.getGu(), request.getDong(), request.getApartmentName())).willReturn(expected);
+
+        // when
+        List<SearchAreaResponse> result = apartmentTransactionService.findAreaForExclusive(request);
+
+        // then
+        Assertions.assertThat(result).isEqualTo(expected);
+        BDDMockito.verify(apartmentTransactionAdapter, BDDMockito.times(1)).findAreaForExclusive(request.getGu(), request.getDong(), request.getApartmentName());
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideNotValidSearchAreaRequest")
+    void findAreaForExclusive_With_Not_Valid_Input(SearchAreaRequest request) {
+        // when & then
+        Assertions.assertThatThrownBy(() -> apartmentTransactionService.findAreaForExclusive(request))
+                .isInstanceOf(IllegalArgumentException.class);
+        BDDMockito.verify(apartmentTransactionAdapter, BDDMockito.times(0)).findAreaForExclusive(request.getGu(), request.getDong(), request.getApartmentName());
+    }
+
+
+    public static Stream<Arguments> provideNotValidSearchCondition() {
+        return Stream.of(
+                Arguments.of(100L, new SearchCondition(Gu.마포구, null, "마포센트럴 아이파크", 111.1083, null, null, Reliability.ALL)),
+                Arguments.of(100L, new SearchCondition(Gu.마포구, "아현동", null, 111.1083, null, null, Reliability.ALL)),
+                Arguments.of(200L, new SearchCondition(Gu.NONE, "아현동", "마포센트럴 아이파크", null, null, null, Reliability.ALL)),
+                Arguments.of(300L, new SearchCondition(Gu.NONE, "아현동", "마포센트럴 아이파크", 111.1083, null, null, null))
+        );
+    }
+
+    public static Stream<Arguments> provideNotValidSearchApartNameRequest() {
+        return Stream.of(
+                Arguments.of(new SearchApartNameRequest(Gu.마포구, null)),
+                Arguments.of(new SearchApartNameRequest(Gu.마포구, " ")),
+                Arguments.of(new SearchApartNameRequest(null, "아현동")),
+                Arguments.of(new SearchApartNameRequest(Gu.NONE, null))
+        );
+    }
+
+    public static Stream<Arguments> provideNotValidSearchAreaRequest() {
+        return Stream.of(
+                Arguments.of(new SearchAreaRequest(Gu.마포구, null, "마포센트럴 아이파크")),
+                Arguments.of(new SearchAreaRequest(Gu.마포구, "아현동", null)),
+                Arguments.of(new SearchAreaRequest(null, "아현동", "마포센트럴 아이파크")),
+                Arguments.of(new SearchAreaRequest(Gu.NONE, "아현동", "마포센트럴 아이파크"))
+        );
+    }
 }
