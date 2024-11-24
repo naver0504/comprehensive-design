4865c2cab62acf85dce773cb6bea7bda7db99b52
naver0504 <qortndwls1234@gmail.com>
2024-11-25 04:18:58 +0900

    feat: findApartmentTransactionsForGraph findApartmentTransactionById 구현

--- src/main/java/com/example/comprehensivedegisn/adapter/ApartmentTransactionAdapter.java
+++ src/main/java/com/example/comprehensivedegisn/adapter/ApartmentTransactionAdapter.java
@@ -10,14 +10,16 @@ import com.example.comprehensivedegisn.dto.response.SearchResponseRecord;
 import com.example.comprehensivedegisn.dto.response.TransactionDetailResponse;
 import org.springframework.data.domain.Page;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 public interface ApartmentTransactionAdapter {
 
-    Optional<ApartmentTransaction> findById(Long id);
+    Optional<ApartmentTransaction> findApartmentTransactionById(Long id);
     Page<SearchResponseRecord> searchApartmentTransactions(Long cachedCount, SearchCondition searchCondition, CustomPageable customPageable);
     List<SearchApartNameResponse> findApartmentNames(Gu gu, String dongName);
     List<SearchAreaResponse> findAreaForExclusive(Gu gu, String dongName, String apartmentName);
     Optional<TransactionDetailResponse> findTransactionDetail(long id);
+    List<ApartmentTransaction> findApartmentTransactionsForGraph(Gu gu, String dongName, String apartmentName, double areaForExclusiveUse, LocalDate startDate, LocalDate endDate);
 }
