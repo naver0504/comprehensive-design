0ea5e6053e31e110280c3867177af3ea8bdb36f6
naver0504 <qortndwls1234@gmail.com>
2025-03-14 06:56:08 +0900

    feat: ASC -> DESC 변경 (Index Desc로 선언함.)

--- src/main/java/com/example/comprehensivedegisn/adapter/order/CustomPageable.java
+++ src/main/java/com/example/comprehensivedegisn/adapter/order/CustomPageable.java
@@ -5,24 +5,26 @@ import com.querydsl.core.types.dsl.ComparableExpressionBase;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@ToString
 public class CustomPageable {
 
     public CustomPageable(Order order, OrderType orderType) {
-        this(Order.ASC, orderType, 0);
+        this(Order.DESC, orderType, 0);
     }
     public CustomPageable(OrderType orderType, int page) {
-        this(Order.ASC, orderType, page);
+        this(Order.DESC, orderType, page);
     }
 
     public static final int DEFAULT_SIZE = 12;
 
-    private Order order = Order.ASC;
+    private Order order = Order.DESC;
     private OrderType orderType = OrderType.DEAL_DATE;
     private int page = 0;
 
